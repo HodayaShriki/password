ca1e7ca01b974bb276435fbed0db9e97a58e9862
Hodaya Shriki <hodayashriki1996@gmail.com>
2022-12-12 13:08:57 +0200

    pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
 
 function App() {
+  const [pass,setpass] = useState("")
+  const [strenth, check] = useState("")
+
+  function handlepass(e){
+    setpass((e.target.value))
+    if ((pass.length)<3)
+    check ("weak")
+    if ((pass.length)>2) 
+    check ("medium")
+    if ((pass.length)>6)
+    check ("strong")
+    }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type = "string" value = {pass} onChange ={handlepass}/>
+      <button disabled={((pass.length)<3)}> Submit</button>
+      <h1>{strenth}</h1>
     </div>
   );
 }
